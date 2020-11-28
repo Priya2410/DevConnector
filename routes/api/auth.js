dd9cd2292a7f586dbcc13b54a5439e642af289c5
priya mohata <mohatapriya@gmail.com>
2020-11-28 11:02:46 +0530

    till lecture 13

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,23 @@
 /*For routing of different pages */
 const express=require('express')
 const router=express.Router();
-
+const auth=require('../middleware/auth')
+const User=require('../../models/User')
 /* @route GET api/auth
 Description of the route : Test Route 
 Access of the route      :Public
 (the access of the route can be public or private in private we
     send the token along like for authentication) */
-router.get('/',function(request,response){
-    response.send('Auth Router')
+router.get('/',auth,async function(request,response){
+    // response.send('Auth Router')
+    try{
+        const user= await User.findById(request.user.id).select('-password');
+        response.json(user);
+    }
+    catch(err){
+        console.error(err.message);
+        response.status(500).send('Server error');
+    }
 })
 
 module.exports=router;
